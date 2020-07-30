a6105549161fb621b1012e3030ae9f205ce41b03
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-07-29 22:54:00 -0700

    Implemented new reducer for board state to decouple it from controller state - seems to have sped things up a bit

---
+++ src/store/reducers/boardReducer.js
@@ -0,0 +1,30 @@
+// import update from "immutability-helper";
+import { UPDATE_BOARD, CLICK_CELL } from "../actions";
+
+const initialState = null;
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UPDATE_BOARD:
+            return action.payload;
+        // case CLICK_CELL:
+        //     return update(state, {
+        //         [action.row]: {
+        //             [action.col]: {
+        //                 $apply: (cell) => !cell,
+        //             },
+        //         },
+        //     });
+        case CLICK_CELL:
+            return [
+                ...state.slice(0, action.row),
+                [
+                    ...state[action.row].slice(0, action.col),
+                    !state[action.row][action.col],
+                    ...state[action.row].slice(action.col + 1),
+                ],
+                ...state.slice(action.row + 1),
+            ];
+        default:
+            return state;
+    }
+};
